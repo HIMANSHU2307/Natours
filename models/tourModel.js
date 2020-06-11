98026ef73f184f2aacc200bfffb20f8bd7f6bbae
HIMANSHU2307 <hvs2394@gmail.com>
2020-06-11 23:49:24 +0530

    Document Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,8 @@
 /* eslint-disable prettier/prettier */
 const mongoose = require('mongoose');
 
+const slugify = require('slugify');
+
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -8,6 +10,7 @@ const tourSchema = new mongoose.Schema({
     unique: true,
     trim: true
   },
+  slug: String,
   duration: {
     type: Number,
     required: [true, 'A tour duration is must!'],
@@ -64,6 +67,13 @@ const tourSchema = new mongoose.Schema({
 tourSchema.virtual('duratioWeeks').get(function () {
   return this.duration / 7; // here this is pointing to current document
   // CAN'T USE VIRTUALPROPERTIES IN QUERY
+});
+
+// MIDDLEWARE DOCUMENT: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+  // console.log(this); // this is the current document
+  this.slug = slugify(this.name, { lower: true });
+  next();
 })
 
 const Tour = mongoose.model('Tour', tourSchema);
