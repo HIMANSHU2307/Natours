41dc2de737caab9ba778ccd16bd192e4cc701c27
HIMANSHU2307 <hvs2394@gmail.com>
2020-06-12 11:21:16 +0530

    Data Validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,71 +2,94 @@
 const mongoose = require('mongoose');
 
 const slugify = require('slugify');
+const validator = require('validator');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour name is must!'],
-    unique: true,
-    trim: true
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour name is must!'],
+      unique: true,
+      trim: true,
+      maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [10, 'A tour name must have more or equal than 10 characters'],
+      validate: [validator.isAlpha, 'Tour name must only contain characters']
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'A tour duration is must!'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour Group Size is must!'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour difficulty is must!'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty can be only easy, medium or difficult'
+      },
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0' ],
+      max: [5, 'Rating must be above 5.0' ]
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A price is must'],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+          // this refers to the current document on NEW document
+        },
+        message: 'Discount price ({VALUE}) should be below the regular price'
+      }
+    },
+    summary: {
+      type: String,
+      required: [true, 'A tour must have a summary'],
+      trim: true, // will remove spaces
+    },
+    description: {
+      type: String,
+      trim: true, // will remove spaces
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
-  slug: String,
-  duration: {
-    type: Number,
-    required: [true, 'A tour duration is must!'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour Group Size is must!'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour difficulty is must!'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A price is must'],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    required: [true, 'A tour must have a summary'],
-    trim: true // will remove spaces
-  },
-  description: {
-    type: String,
-    trim: true // will remove spaces
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String],
-  createAt: {
-    type: Date,
-    default: Date.now()
-  },
-  startDates: [Date],
-  secretTour: {
-    type: Boolean,
-    default: false
-  }
-}, { // OPTION TO SHOW OR NOT THE VIRTUAL PROPERTIES OF SCHEMA
+  {
+    // OPTION TO SHOW OR NOT THE VIRTUAL PROPERTIES OF SCHEMA
     toJSON: { virtuals: true },
-  toObject: { virtuals: true }
-});
+    toObject: { virtuals: true },
+  }
+);
 
 tourSchema.virtual('duratioWeeks').get(function () {
   return this.duration / 7; // here this is pointing to current document
